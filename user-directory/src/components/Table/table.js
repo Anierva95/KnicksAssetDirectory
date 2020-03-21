bb05e54fc7bcbd58cdaedbf5fd24e4f10050bd89
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-21 19:45:32 -0400

    added hover effect to all buttons

--- user-directory/src/components/Table/table.js
+++ user-directory/src/components/Table/table.js
@@ -4,18 +4,49 @@ import TableRows from '../TableRows/Tablerows'
 function Table(props) {
 
    const [sortAsset, updateSortAsset] = useState(false);
+   const [sortFirst, updateSortFirst] = useState(false);
+   const [sortLast, updateSortLast] = useState(false);
+   const [sortPosition, updateSortPosition] = useState(false);
 
    const toggleSortAsset = () => {
      updateSortAsset(!sortAsset)
-
    }
-
-   let toggleClassAsset;
-   if (sortAsset) {
-    toggleClassAsset = 'orange';
-  } else {
-    toggleClassAsset = 'unToggled';
+   const toggleSortFirst = () => {
+    updateSortFirst(!sortFirst)
   }
+  const toggleSortLast = () => {
+    updateSortLast(!sortLast)
+  }
+  const toggleSortPosition = () => {
+    updateSortPosition(!sortPosition)
+  }
+  let toggleClassAsset;
+  let toggleClassFirst;
+  let toggleClassLast;
+  let toggleClassPosition;
+
+  if (sortAsset) {
+   toggleClassAsset = 'orange';
+ } else {
+   toggleClassAsset = 'unToggled';
+ }
+ if (sortFirst) {
+  toggleClassFirst = 'orange';
+} else {
+  toggleClassFirst = 'unToggled';
+}
+if (sortLast) {
+  toggleClassLast = 'orange';
+} else {
+  toggleClassLast = 'unToggled';
+}
+if (sortPosition) {
+  toggleClassPosition = 'orange';
+} else {
+  toggleClassPosition = 'unToggled';
+}
+
+
 
 
     return(
@@ -23,9 +54,9 @@ function Table(props) {
         <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Position</th>
+      <th scope="col" name="sortFirst" onClick={toggleSortFirst} className={toggleClassFirst}>First Name</th>
+      <th scope="col" name="sortLast" onClick={toggleSortLast} className={toggleClassLast}>Last Name</th>
+      <th scope="col" name="sortPosition" onClick={toggleSortPosition} className={toggleClassPosition}>Position</th>
       <th scope="col" onClick={toggleSortAsset} className={toggleClassAsset}>Asset Rank</th>
       <th scope="col">Description</th>
     </tr>
