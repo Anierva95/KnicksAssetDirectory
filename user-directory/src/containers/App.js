4a91f3e14a0759ca5e2ac928ad96799630fd8b81
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-21 09:18:39 -0400

    Fixed some styling, ready to populate with some more data

--- user-directory/src/containers/App.js
+++ user-directory/src/containers/App.js
@@ -4,12 +4,25 @@ import Header from '../components/Header/header'
 import Table from '../components/Table/table'
 
 function App() {
+
+  const style ={
+    marginTop: "10px",
+    marginBottom: "18px"
+  }
   return (
     <div className="App">
-        <Header/>
+        <Header
+        />
+        <div className="nameSearch">
+        <h1>Search by Name: </h1>
+        <input 
+        style={style}
+        />
+        </div>
         <body>
         </body>
-        <Table/>
+        <Table
+        />
 
 
     </div>
