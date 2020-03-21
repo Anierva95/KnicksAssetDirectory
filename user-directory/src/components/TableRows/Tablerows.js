8944b0c2def839c490720da3a220c657e49ac35d
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-21 16:51:16 -0400

    sorted function on click now works

--- user-directory/src/components/TableRows/Tablerows.js
+++ user-directory/src/components/TableRows/Tablerows.js
@@ -5,9 +5,25 @@ function TableRows(props) {
 
   let filteredByName = players.filter(player => {
     return (player.First_Name.toUpperCase().indexOf(props.playerSearch.toUpperCase()) === 0 || player.Last_Name.toUpperCase().indexOf(props.playerSearch.toUpperCase()) === 0 );
-  })
+  });
 
-  let playerRows = filteredByName.map(player => {
+  let sorted;
+
+  if (props.sortAssets) {
+  sorted = filteredByName.sort((a,b) => {
+    if (a.Asset_Rank > b.Asset_Rank) {
+      return 1
+    } else if (a.Asset_Rank < b.Asset_Rank) {
+      return -1
+    }
+    return 0;
+  })} else {
+    sorted = filteredByName;
+  };
+
+
+
+  let playerRows = sorted.map(player => {
     return (
       <tr>
       <td>{player.First_Name}</td>
