4dec1548bbc211f532adb3be6c207f3599738d72
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-21 09:40:57 -0400

    player rows successfully mapped from JSON

--- user-directory/src/components/TableRows/Tablerows.js
+++ user-directory/src/components/TableRows/Tablerows.js
@@ -3,16 +3,21 @@ import players from "../../players.json"
 
 function TableRows() {
 
-  
+  let playerRows = players.map(player => {
+    return (
+      <tr>
+      <td>{player.First_Name}</td>
+      <td>{player.Last_Name}</td>
+      <td>{player.Position}</td>
+      <td>{player.Asset_Rank}</td>
+      <td>{player.Description}</td>
+        </tr>
+    )
+  })
+
     return(
       <React.Fragment>
-        <tr>
-    <td>{players[0].First_Name}</td>
-        <td>{players[0].First_Name}</td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
+        {playerRows}
       </React.Fragment>
     )
 }
