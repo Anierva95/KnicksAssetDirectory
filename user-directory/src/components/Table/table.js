14acad7b909d2ec6f48908e285a6553173f3fd26
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-21 16:04:02 -0400

    enable toggling props

--- user-directory/src/components/Table/table.js
+++ user-directory/src/components/Table/table.js
@@ -7,9 +7,8 @@ function Table(props) {
 
    const toggleSortAsset = () => {
      updateSortAsset(!sortAsset)
+
    }
-
-
 
     return(
     <div class="container table">
@@ -19,13 +18,14 @@ function Table(props) {
       <th scope="col">First Name</th>
       <th scope="col">Last Name</th>
       <th scope="col">Position</th>
-      <th scope="col" onClick={toggleSortAsset}>Asset Rank</th>
+      <th scope="col" onClick={toggleSortAsset} className='orange'>Asset Rank</th>
       <th scope="col">Description</th>
     </tr>
   </thead>
   <tbody>
       <TableRows
       playerSearch={props.playerSearch}
+      sortAssets={sortAsset}
       />
   </tbody>
 </table>
